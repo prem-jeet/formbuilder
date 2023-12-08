08d98481cc9491811bc9602ffde713270a88e862
prem-jeet <jeet4300795@gmail.com>
2023-12-08 08:30:30 +0530

    added api endpoint to local env file

--- app/enums/new/formAction.ts
+++ app/enums/new/formAction.ts
@@ -2,7 +2,7 @@
 
 import { EnumFormSchema } from "@/validationSchemas/validationSchemas";
 
-const API_EndPOINT = "http://127.0.0.1:8090/api/collections/enums/records";
+const API_ENDPOINT = process.env.API_ENDPOINT;
 
 interface Enum {
   name: string;
@@ -16,33 +16,35 @@ export const createEnum = async (newEnum: Enum) => {
     return { error: true, msg: "validation error" };
   }
 
-  try {
-    const rsp = await fetch(API_EndPOINT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newEnum),
-      cache: "no-store",
-    });
-    const res = await rsp.json();
+  if (API_ENDPOINT) {
+    try {
+      const rsp = await fetch(API_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newEnum),
+        cache: "no-store",
+      });
+      const res = await rsp.json();
 
-    if (res.message) {
-      const error = res.data as { [key: string]: { message: string } };
-      return {
-        error: true,
-        msg: Object.entries(error).reduce(
-          (acc, entry) => acc + `${entry[0]}  ${entry[1].message}\n`,
-          ""
-        ),
-      };
-    } else {
-      return { ...res, error: false, msg: "" } as Enum & {
-        error: boolean;
-        msg: string;
-      };
+      if (res.message) {
+        const error = res.data as { [key: string]: { message: string } };
+        return {
+          error: true,
+          msg: Object.entries(error).reduce(
+            (acc, entry) => acc + `${entry[0]}  ${entry[1].message}\n`,
+            ""
+          ),
+        };
+      } else {
+        return { ...res, error: false, msg: "" } as Enum & {
+          error: boolean;
+          msg: string;
+        };
+      }
+    } catch (e) {
+      return { error: true, msg: "some unexpected error occured!" };
     }
-  } catch (e) {
-    return { error: true, msg: "some unexpected error occured!" };
   }
 };
