57d782f5ff65e3faf4e2887029729afc7ef5a336
prem-jeet <jeet4300795@gmail.com>
2023-12-12 13:13:39 +0530

    sorting added for name and labels

--- app/enums/columns.tsx
+++ app/enums/columns.tsx
@@ -2,15 +2,45 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Enum } from "./new/formAction";
+import { FaSortAlphaDown, FaSortAlphaDownAlt } from "react-icons/fa";
 
 export const columns: ColumnDef<Enum>[] = [
   {
     accessorKey: "name",
-    header: "Name",
+
+    header: ({ column }) => {
+      return (
+        <button
+          className="btn btn-ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          {column.getIsSorted() === "asc" ? (
+            <FaSortAlphaDown className="ml-2 h-4 w-4" />
+          ) : (
+            <FaSortAlphaDownAlt className="ml-2 h-4 w-4" />
+          )}
+        </button>
+      );
+    },
   },
   {
     accessorKey: "label",
-    header: "Label",
+    header: ({ column }) => {
+      return (
+        <button
+          className="btn btn-ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Label
+          {column.getIsSorted() === "asc" ? (
+            <FaSortAlphaDown className="ml-2 h-4 w-4" />
+          ) : (
+            <FaSortAlphaDownAlt className="ml-2 h-4 w-4" />
+          )}
+        </button>
+      );
+    },
   },
   {
     accessorKey: "options",
